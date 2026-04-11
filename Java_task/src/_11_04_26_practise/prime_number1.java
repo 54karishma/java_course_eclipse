747888a218a34b7f60d8ad609417cfcf70f4e6af
user <user@DESKTOP-6FUMEEV>
2026-04-11 09:27:01 +0530

    prime number

---
+++ Java_task/src/_11_04_26_practise/prime_number1.java
@@ -0,0 +1,26 @@
+package _11_04_26_practise;
+import java.util.Scanner;
+
+public class prime_number1 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int count=0;
+		for(int i=1;i<=a;i++) {
+			if(a%i==0) {
+				count++;
+			}
+			
+		}
+		if(count==2) {
+			System.out.println("prime number");
+		}
+		else {
+			System.out.println("not a prime number");
+		}
+		
+
+	}
+
+}
